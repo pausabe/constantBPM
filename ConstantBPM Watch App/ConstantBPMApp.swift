1e060e1d06d2ce65f65bf7a90137b234e42e41aa
Pau Sabé <pau.sabe@icloud.com>
2022-09-26 10:15:14 +0200

    Initial Commit

---
+++ ConstantBPM Watch App/ConstantBPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantBPMApp.swift
+//  ConstantBPM Watch App
+//
+//  Created by Pau Sabé Martínez on 26/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ConstantBPM_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConstantBPM Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConstantBPM Watch App
+//
+//  Created by Pau Sabé Martínez on 26/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
